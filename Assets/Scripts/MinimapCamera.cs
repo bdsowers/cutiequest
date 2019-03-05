c1d59d32894c867bb45827735a7eb569fd98ceb4
Brian Sowers <bdsowers@gmail.com>
2019-03-05 16:43:36 -0500

    Minimap support

---
+++ Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = target.transform.position;
+        pos.y = transform.position.y;
+        transform.position = pos;
+    }
+}
