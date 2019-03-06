36e18f51d96a70553c5aea6fd9b6ca307cfddba4
Brian Sowers <bdsowers@gmail.com>
2019-03-06 17:41:06 -0500

    Basic title screen

---
+++ Assets/Scripts/TitleHeart.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleHeart : MonoBehaviour
+{
+    private float mDelay = 0f;
+    private float mAnimationTimer = 0f;
+    private float mStartRotation = 0f;
+
+    private float mDisappearTimer = -1f;
+    private float mOriginalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mDelay = Random.Range(0f, 20f);
+        mStartRotation = 0f;
+        mOriginalScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mDisappearTimer >= 0f)
+        {
+            mDisappearTimer += Time.deltaTime * 3f;
+            mDisappearTimer = Mathf.Min(1f, mDisappearTimer);
+            transform.localScale = Vector3.Lerp(Vector3.one * mOriginalScale, Vector3.zero, mDisappearTimer);
+        }
+        else if (mDelay >= 0f)
+        {
+            mDelay -= Time.deltaTime;
+            mAnimationTimer = 0f;
+            mStartRotation = transform.rotation.eulerAngles.y;
+        }
+        else
+        {
+            mAnimationTimer += Time.deltaTime * 0.25f;
+            if (mAnimationTimer > 1f)
+            {
+                mAnimationTimer = 1f;
+                mDelay = Random.Range(0f, 20f);
+            }
+            transform.localRotation = Quaternion.Euler(0f, mStartRotation + 180f * mAnimationTimer, 0f);
+        }
+        
+    }
+
+    public void MakeDisappear()
+    {
+        mDisappearTimer = 0.01f;
+    }
+}
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject heartPrefab;
+    public GameObject titleContainer;
+
+    private bool mLeaving = false;
+    private float mLeaveTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = -5; x <= 5; ++x)
+        {
+            for (int y = -5; y <= 5; ++y)
+            {
+                float offset = 0f;
+
+                if (y <= 0)
+                    offset = -1f;
+
+                Vector3 pos = new Vector3(x * 4f, offset + y * 4f, 0f);
+                GameObject newHeart = GameObject.Instantiate(heartPrefab);
+                newHeart.transform.position = pos;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            Leave();
+
+        if (mLeaving)
+        {
+            mLeaveTimer += Time.deltaTime * 2f;
+            mLeaveTimer = Mathf.Min(mLeaveTimer, 1f);
+            titleContainer.transform.localScale = new Vector3(1f - mLeaveTimer, 1f, 1f);
+        }
+    }
+
+    void Leave()
+    {
+        if (mLeaving)
+            return;
+
+        mLeaving = true;
+
+        TitleHeart[] hearts = GameObject.FindObjectsOfType<TitleHeart>();
+        for (int i = 0; i < hearts.Length; ++i)
+        {
+            hearts[i].MakeDisappear();
+        }
+
+        Invoke("MoveToNextScene", 0.6f);
+    }
+
+    void MoveToNextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
