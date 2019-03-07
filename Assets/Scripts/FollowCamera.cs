6ceefaed4fdbb82cce8f3d9edc7cea3b815844bd
Brian Sowers <bdsowers@gmail.com>
2019-03-07 15:32:54 -0500

    Forward progress on the HUB scene

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,6 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     public GameObject target;
     public Vector3 offset;
+    public bool useCinematicOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,18 @@ public class FollowCamera : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = target.transform.position + offset;
+        Vector3 cinematicOffset = Vector3.zero;
+        if (useCinematicOffset)
+        {
+            if (target.transform.position.z >= 0f)
+            {
+                cinematicOffset.y = -target.transform.position.z * 0.5f;
+                if (cinematicOffset.y < -2.5f)
+                    cinematicOffset.y = -2.5f;
+            }
+        }
+
+        transform.position = target.transform.position + offset + cinematicOffset;
         transform.LookAt(target.transform);
     }
 }
