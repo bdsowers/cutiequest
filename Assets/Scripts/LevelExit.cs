95e9a1ce889b9854a779a1fbecbfaa93ac9e932a
Brian Sowers <bdsowers@gmail.com>
2019-03-14 17:31:42 -0400

    Making the level transition prefab a little cleaner

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class LevelExit : MonoBehaviour
 {
+    private bool mIsTransitioning;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (mIsTransitioning)
+            return;
+
         if (other.GetComponentInParent<PlayerController>() != null)
         {
-            // todo bdsowers - indicate movement to the next floor in some fashion; likely on the Game object
-
-            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+            mIsTransitioning = true;
+            Invoke("Transition", 0.5f);
         }
+    }
+
+    private void Transition()
+    {
+        
+        // todo bdsowers - indicate movement to the next floor in some fashion; likely on the Game object
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
     }
 }
