b6bb77d6022dbd2d9b90ed267eb24eabecd2c40b
Brian Sowers <bdsowers@gmail.com>
2019-03-28 14:47:32 -0400

    Improved spell casting

--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class SpellCaster : MonoBehaviour
 {
     public int targetDeviation = 0;
+    public bool targetCaster;
+    public bool orientToDirection;
+
+    [TextArea(8, 10)]
+    public string patternString;
+
     public bool isCasting { get; private set; }
 
-    private int[,] mPattern = new int[,]
-    {
-        { 2, 2, 2, 2, 2 },
-        { 2, 1, 1, 1, 2 },
-        { 2, 1, 0, 1, 2 },
-        { 2, 1, 1, 1, 2 },
-        { 2, 2, 2, 2, 2 }
-    };
+    private int[,] mPattern = null;
 
     private CollisionMap mCollisionMap;
+    private float timeScale = 4f;
 
     private void Start()
     {
         mCollisionMap = GameObject.FindObjectOfType<CollisionMap>();
+        mPattern = ParsePatternString(patternString);
+    }
+
+    private int[,] ParsePatternString(string str)
+    {
+        string[] lines = str.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        int[,] result = new int[lines[0].Length, lines.Length];
+        
+        for (int x = 0; x < lines[0].Length; ++x)
+        {
+            for (int y = 0; y < lines.Length; ++y)
+            {
+                result[x, y] = int.Parse(lines[y][x].ToString());
+            }
+        }
+
+        return result;
     }
 
     private void Update()
@@ -56,6 +73,12 @@ public class SpellCaster : MonoBehaviour
             spellZ += Random.Range(-targetDeviation, targetDeviation + 1);
         }
 
+        if (targetCaster)
+        {
+            spellX = Mathf.RoundToInt(transform.position.x);
+            spellZ = Mathf.RoundToInt(transform.position.z);
+        }
+
         spellZ = -spellZ;
 
         int currentPart = 1;
@@ -64,7 +87,7 @@ public class SpellCaster : MonoBehaviour
         {
             keepCasting = CastSpellPart(currentPart, spellX, spellZ);
             ++currentPart;
-            yield return new WaitForSeconds(1.75f);
+            yield return new WaitForSeconds(1.75f / timeScale);
         }
 
         isCasting = false;
@@ -79,6 +102,9 @@ public class SpellCaster : MonoBehaviour
         int patternWidth = mPattern.GetLength(0);
         int patternHeight = mPattern.GetLength(1);
 
+        GameObject spellContainer = new GameObject();
+        spellContainer.transform.position = transform.position;
+
         for (int x = 0; x < patternWidth; ++x)
         {
             for (int z = 0; z < patternHeight; ++z)
@@ -89,16 +115,26 @@ public class SpellCaster : MonoBehaviour
                 int targetX = spellX + offsetX;
                 int targetZ = spellZ + offsetZ;
 
-                if (mPattern[x, z] == currentPart && mCollisionMap.SpaceMarking(targetX, targetZ) != 1)
+                if (mPattern[x, z] == currentPart)
                 {
-                    GameObject target = GameObject.Instantiate(PrefabManager.instance.PrefabByName("SpellTarget"));
+                    GameObject target = GameObject.Instantiate(PrefabManager.instance.PrefabByName("SpellTarget"), spellContainer.transform);
                     target.transform.position = new Vector3(targetX, 1f, -targetZ);
-                    target.GetComponent<SpellTarget>().castTime = 1f;
+                    target.GetComponent<SpellTarget>().castTime = 1f * (1f / timeScale);
                     target.GetComponent<SpellTarget>().effect = "Explosion";
 
                     partFound = true;
                 }
             }
+        }
+
+        if (orientToDirection)
+        {
+            spellContainer.transform.localRotation = GetComponentInChildren<Animator>().transform.localRotation;
+
+            // Round the Y rotation to the nearest 90 degree interval; root motion makes the rotation a little imprecise.
+            float y = spellContainer.transform.localRotation.eulerAngles.y;
+            y = Mathf.Round(y / 90) * 90.0f;
+            spellContainer.transform.localRotation = Quaternion.Euler(spellContainer.transform.localRotation.eulerAngles.x, y, spellContainer.transform.localRotation.eulerAngles.z);
         }
 
         return partFound;
