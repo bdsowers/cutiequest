5c70856530ef5ed084b81bd5f54fe3e3764f80e2
Brian Sowers <bdsowers@gmail.com>
2019-04-09 18:35:15 -0400

    Fire wall improvements

---
+++ Assets/Scripts/DestroyLoopingParticleSystemAfterTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLoopingParticleSystemAfterTime : MonoBehaviour
+{
+    public float time;
+    private float mTimer;
+
+    private void Start()
+    {
+        mTimer = time;
+    }
+
+    private void Update()
+    {
+        mTimer -= Time.deltaTime;
+        if (mTimer < 0f)
+        {
+            ParticleSystem[] particleSystems = GetComponentsInChildren<ParticleSystem>();
+            for (int i = 0; i < particleSystems.Length; ++i)
+            {
+                ParticleSystem.MainModule module = particleSystems[i].main;
+                module.loop = false;
+            }
+        }
+
+        if (mTimer < -3f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
