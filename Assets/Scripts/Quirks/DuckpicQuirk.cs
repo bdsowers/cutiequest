c70a2d07c8fd1adf35d81769f72e05bb6bebbe01
Brian Sowers <bdsowers@gmail.com>
2019-04-10 17:40:00 -0400

    Functional duckpic quirk, still needs a little tweaking

--- Assets/Scripts/Quirks/DuckpicQuirk.cs
+++ Assets/Scripts/Quirks/DuckpicQuirk.cs
@@ -4,4 +4,31 @@ using UnityEngine;
 
 public class DuckpicQuirk : Quirk
 {
+    private float mTimer;
+
+    private void Start()
+    {
+        mTimer = Random.Range(30, 120);
+    }
+
+    private void Update()
+    {
+        mTimer -= Time.deltaTime;
+
+        if (mTimer < 0f)
+        {
+            mTimer = Random.Range(30, 120);
+
+            DungeonCanvas dc = GameObject.FindObjectOfType<DungeonCanvas>();
+            if (dc != null)
+            {
+                ShowDuckpic(dc);
+            }
+        }
+    }
+
+    void ShowDuckpic(DungeonCanvas canvas)
+    {
+        canvas.duckpicUI.gameObject.SetActive(true);
+    }
 }
---
+++ Assets/Scripts/Quirks/DuckpicUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ArrayExtensions;
+
+public class DuckpicUI : MonoBehaviour
+{
+    public Text message;
+    public Image image;
+
+    string[] messages = new string[]
+    {
+        "Check out this huge duck. Like what you see?",
+        "On a scale from like it to love it, plz rate this duck.",
+        "Have you ever seen a duck this great before?",
+        "Would you like to pet the duck?",
+        "Hey why aren't you responding to my duck pics?",
+    };
+
+    public void OnEnable()
+    {
+        message.text = messages.Sample();
+    }
+
+    public void OnClosePressed()
+    {
+        gameObject.SetActive(false);
+    }
+}
