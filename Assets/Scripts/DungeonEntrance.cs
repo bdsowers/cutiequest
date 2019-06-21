b9aa8152b1bda2dfc7024fc6be9d3b0ccc9bb457
Brian Sowers <bdsowers@gmail.com>
2019-06-21 15:55:43 -0400

    No longer allowing player in dungeon without a companion

--- Assets/Scripts/DungeonEntrance.cs
+++ Assets/Scripts/DungeonEntrance.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class DungeonEntrance : MonoBehaviour
 {
     public DungeonData dungeonData;
+    public GameObject visualDungeonEntrance;
+
+    private bool mActive = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponentInParent<PlayerController>() != null)
         {
-            EnterDungeon();
+            if (Game.instance.followerData != null)
+            {
+                EnterDungeon();
+            }
         }
     }
 
@@ -21,5 +27,20 @@ public class DungeonEntrance : MonoBehaviour
         Game.instance.EnterDungeon(dungeonData);
 
         Game.instance.transitionManager.TransitionToScreen("Dungeon");
+    }
+
+    private void Update()
+    {
+        if (mActive && Game.instance.followerData == null)
+        {
+            mActive = false;
+            visualDungeonEntrance.SetActive(true);
+        }
+
+        if (!mActive && Game.instance.followerData != null)
+        {
+            mActive = true;
+            visualDungeonEntrance.SetActive(false);
+        }
     }
 }
