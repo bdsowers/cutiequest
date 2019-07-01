a946ef687282d85f7d36c3168fdde19014421eeb
Brian Sowers <bdsowers@gmail.com>
2019-07-01 17:04:10 -0400

    Guillotine shrine

---
+++ Assets/Scripts/Shrines/ShrineBomb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrineBomb : Shrine
+{
+    protected override IEnumerator ActivationCoroutine()
+    {
+        yield return base.ActivationCoroutine();
+
+        if (WasAccepted())
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Shrines/ShrineGuillotine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrineGuillotine : Shrine
+{
+    protected override IEnumerator ActivationCoroutine()
+    {
+        yield return base.ActivationCoroutine();
+
+        if (WasAccepted())
+        {
+            int val = Random.Range(0, 2);
+            if (val == 0)
+            {
+                NumberPopupGenerator.instance.GeneratePopup(transform.position + Vector3.up * 0.7f, "All enemies destroyed", NumberPopupReason.Heal);
+
+                Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+                for (int i = 0; i < enemies.Length; ++i)
+                {
+                    Killable killable = enemies[i].GetComponent<Killable>();
+                    killable.TakeDamage(killable.health);
+                }
+            }
+            else
+            {
+                NumberPopupGenerator.instance.GeneratePopup(transform.position + Vector3.up * 0.7f, "The odds were not in your favor", NumberPopupReason.TakeDamage);
+
+                Game.instance.playerData.health = 1;
+                Game.instance.avatar.GetComponent<Killable>().health = 1;
+            }
+        }
+    }
+}
