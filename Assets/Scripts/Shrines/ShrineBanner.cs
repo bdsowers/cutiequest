e8ea43a3c5c1bc295901a574b6fac981966ac19f
Brian Sowers <bdsowers@gmail.com>
2019-07-01 14:18:19 -0400

    New shrine implemented

---
+++ Assets/Scripts/Shrines/ShrineBanner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrineBanner : Shrine
+{
+    protected override IEnumerator ActivationCoroutine()
+    {
+        yield return base.ActivationCoroutine();
+
+        if (WasAccepted())
+        {
+            Game.instance.currentDungeonFloor = 5;
+            Game.instance.transitionManager.TransitionToScreen("Dungeon");
+        }
+    }
+}
