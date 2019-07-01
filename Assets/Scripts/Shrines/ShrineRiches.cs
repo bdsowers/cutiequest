ade1ea6b3148a10e7fc65d416ab84bbb3f99791d
Brian Sowers <bdsowers@gmail.com>
2019-07-01 14:27:01 -0400

    New shrine implemented

---
+++ Assets/Scripts/Shrines/ShrineRiches.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrineRiches : Shrine
+{
+    protected override IEnumerator ActivationCoroutine()
+    {
+        yield return base.ActivationCoroutine();
+
+        if (WasAccepted())
+        {
+            NumberPopupGenerator.instance.GeneratePopup(transform.position + Vector3.one, Game.instance.playerData.numCoins, NumberPopupReason.RemoveCoins);
+            Game.instance.playerData.numCoins = 0;
+
+            CharacterStatModifier followerMod = Game.instance.avatar.follower.GetComponentInChildren<CharacterStatModifier>();
+            followerMod.SetRelativeModification(followerMod.statType, followerMod.modification * 2);
+        }
+    }
+}
