021c9ca4da540d99ab595609e614b7715e49b62c
Brian Sowers <bdsowers@gmail.com>
2019-07-23 11:07:04 -0400

    Basic UI/button sounds

---
+++ Assets/Scripts/ButtonSoundPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonSoundPlayer : MonoBehaviour, ISelectHandler
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnDestroy()
+    {
+        GetComponent<Button>().onClick.RemoveListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        Game.instance.soundManager.PlaySound("button_press");
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Game.instance.soundManager.PlaySound("button_select");
+    }
+}
