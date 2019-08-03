7cede411696b8a6d195e2a5c5a37ff1b3adf7e87
Brian Sowers <bdsowers@gmail.com>
2019-08-03 14:18:19 -0400

    Stubbing in all town NPCs that can be unlocked

---
+++ Assets/Scripts/RandomDance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int dance = Random.Range(1, 5);
+        GetComponentInChildren<Animator>().Play("Dance" + dance);
+    }
+}
