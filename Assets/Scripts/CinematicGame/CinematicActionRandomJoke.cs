3e597f3ec420b9a91fd04a0f6c59e9fdd0e6aed6
Brian Sowers <bdsowers@gmail.com>
2019-08-05 16:23:58 -0400

    Hotdogman implemented

---
+++ Assets/Scripts/CinematicGame/CinematicActionRandomJoke.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicActionRandomJoke : CinematicAction
+{
+    public override string actionName
+    {
+        get { return "random_joke"; }
+    }
+
+    public override IEnumerator PlayInternal(CinematicDirector player)
+    {
+        List<string> setups = LocalizedText.GetKeysInList("[JOKE_SETUP]");
+        List<string> punchlines = LocalizedText.GetKeysInList("[JOKE_PUNCHLINE]");
+
+        int jokeNum = Random.Range(0, setups.Count);
+        string setupKey = setups[jokeNum];
+        string punchlineKey = punchlines[jokeNum];
+        string setup = LocalizedText.Get(setupKey);
+        string punchline = LocalizedText.Get(punchlineKey);
+
+        CinematicActionTypewriter setupAction = new CinematicActionTypewriter();
+        Dictionary<string, string> setupParams = new Dictionary<string, string>();
+        setupParams.Add("text", setup);
+        setupParams.Add("keep_open", "true");
+        setupAction.SetParameters(setupParams);
+        yield return player.PlayAction(setupAction);
+
+        CinematicActionTypewriter punchlineAction = new CinematicActionTypewriter();
+        Dictionary<string, string> punchlineParams = new Dictionary<string, string>();
+        punchlineParams.Add("text", punchline);
+        punchlineAction.SetParameters(punchlineParams);
+        yield return player.PlayAction(punchlineAction);
+
+        yield break;
+    }
+}
