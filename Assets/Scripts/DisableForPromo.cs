1c9c5255632443bf8122562b0a1bfccf04002c4b
Brian Sowers <bdsowers@gmail.com>
2019-08-07 12:30:07 -0400

    UI-free code

---
+++ Assets/Scripts/DisableForPromo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableForPromo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>(true);
+        foreach(Renderer renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+
+        Image[] images = GetComponentsInChildren<Image>(true);
+        foreach(Image image in images)
+        {
+            image.enabled = false;
+        }
+
+        Text[] text = GetComponentsInChildren<Text>(true);
+        foreach(Text label in text)
+        {
+            label.enabled = false;
+        }
+
+        RawImage[] rawImages = GetComponentsInChildren<RawImage>(true);
+        foreach(RawImage image in rawImages)
+        {
+            image.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
