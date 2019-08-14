a4b55d0725b283c06f49c18651b2e6041d4e17e8
Brian Sowers <bdsowers@gmail.com>
2019-08-14 16:09:18 -0400

    Playing around with postprocessing and a new quirk

---
+++ Assets/Scripts/Quirks/TipsyQuirk.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TipsyQuirk : MonoBehaviour
+{
+    LensDistortion mLensDistortion;
+    PostProcessVolume mVolume;
+
+    float mIntensity = 0f;
+    float mDirection = 1f;
+    bool mMovedCenter = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mLensDistortion = ScriptableObject.CreateInstance<LensDistortion>();
+        mLensDistortion.enabled.Override(true);
+        mLensDistortion.intensity.Override(0f);
+        mLensDistortion.centerX.Override(0.1f);
+        mLensDistortion.centerY.Override(0.1f);
+
+        mVolume = PostProcessManager.instance.QuickVolume(LayerMask.NameToLayer("VFXVolume"), 100f, mLensDistortion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float influence = 50f;
+        float speed = 50f;
+
+        mIntensity += mDirection * Time.deltaTime * speed;
+        if (mIntensity > influence || mIntensity < -influence)
+        {
+            mIntensity = Mathf.Clamp(mIntensity, -influence, influence);
+            mDirection = -mDirection;
+            mMovedCenter = false;
+        }
+
+        if (Mathf.Abs(mIntensity) < 5f && !mMovedCenter)
+        {
+            mMovedCenter = true;
+            mLensDistortion.centerX.Override(Random.Range(-0.2f, 0.2f));
+            mLensDistortion.centerY.Override(Random.Range(-0.2f, 0.2f));
+        }
+
+        mLensDistortion.intensity.value = mIntensity;
+    }
+}
