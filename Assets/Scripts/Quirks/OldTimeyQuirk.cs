8c7da0ffc90f30b94aa4c8e4fdef71132004456e
Brian Sowers <bdsowers@gmail.com>
2019-08-16 10:41:28 -0400

    Stubbing in old-time VFX with grayscale and grain, still want an intermission overlay

---
+++ Assets/Scripts/Quirks/OldTimeyQuirk.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class OldTimeyQuirk : Quirk
+{
+    Grayscale mGrayscale;
+    Grain mGrain;
+    PostProcessVolume mVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mGrayscale = ScriptableObject.CreateInstance<Grayscale>();
+        mGrayscale.enabled.Override(true);
+
+        mGrain = ScriptableObject.CreateInstance<Grain>();
+        mGrain.enabled.Override(true);
+        mGrain.intensity.Override(1f);
+        mGrain.size.Override(1.5f);
+        mGrain.lumContrib.Override(1f);
+
+        mVolume = PostProcessManager.instance.QuickVolume(LayerMask.NameToLayer("VFXVolume"), 100f, mGrayscale, mGrain);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        DestroyVolume();
+    }
+
+    private void OnDestroy()
+    {
+        DestroyVolume();
+    }
+
+    void DestroyVolume()
+    {
+        if (mVolume == null)
+            return;
+
+        RuntimeUtilities.DestroyVolume(mVolume, true, true);
+        mVolume = null;
+    }
+}
