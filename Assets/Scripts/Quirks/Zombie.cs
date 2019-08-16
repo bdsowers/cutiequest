664fed94266bea8428db03acac9cf345c2d43801
Brian Sowers <bdsowers@gmail.com>
2019-08-16 12:19:16 -0400

    Adding in assets for zombie quirk, but it's currently turned off as it's been finicky to get working properly:

---
+++ Assets/Scripts/Quirks/Zombie.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    Killable mKillable;
+    CollisionMap mCollisionMap;
+
+    float mReviveTime = 120f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mKillable = GetComponent<Killable>();
+        mCollisionMap = GameObject.FindObjectOfType<CollisionMap>();
+
+        mReviveTime = 10f; //  Random.Range(120f, 240f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mKillable.isDead)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.J))
+            mReviveTime = 0f;
+
+        mReviveTime -= Time.deltaTime;
+        if (mReviveTime < 0f)
+        {
+            // See if we can get up - if something is on top of us, we can't
+            Vector2Int mapPos = MapCoordinateHelper.WorldToMapCoords(transform.position);
+            if (mCollisionMap.SpaceMarking(mapPos.x, mapPos.y) == 0)
+            {
+                mReviveTime = Random.Range(120f, 240f);
+
+                Revive();
+            }
+        }
+    }
+
+    private void Revive()
+    {
+        // mKillable.Revive();
+    }
+}
---
+++ Assets/Scripts/Quirks/ZombieQuirk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// todo bdsowers - this whole thing is cut while I work out a way to get the animations smoother
+public class ZombieQuirk : Quirk
+{
+    void Start()
+    {
+        Game.instance.centralEvents.onEnemyCreated += OnEnemyCreated;
+    }
+
+    private void OnEnemyCreated(Enemy enemy)
+    {
+        if (!enemy.isBoss)
+        {
+            //enemy.GetComponent<Killable>().deathResponse = Killable.DeathResponse.MakeInactiveSilent;
+            enemy.gameObject.AddComponent<Zombie>();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Game.instance.centralEvents.onEnemyCreated -= OnEnemyCreated;
+    }
+}
