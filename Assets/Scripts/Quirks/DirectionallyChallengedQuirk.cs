b16644c669510e2705c5735424cf9ee29b7a18ab
Brian Sowers <bdsowers@gmail.com>
2019-08-19 10:29:04 -0400

    Bad at directions quirk

---
+++ Assets/Scripts/Quirks/DirectionallyChallengedQuirk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DirectionallyChallengedQuirk : Quirk
+{
+    private GameObject mMinimapRawImage;
+    
+    private float mTimer;
+    private int mCurrentRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMinimapRawImage = CinematicId.FindObjectWithId("minimap_image");
+        mTimer = Random.Range(10f, 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mMinimapRawImage == null)
+            return;
+
+        // todo bdsowers - performance yuck
+        GameObject fullMap = CinematicId.FindObjectWithId("full_map_image");
+        if (fullMap != null)
+            fullMap.transform.rotation = mMinimapRawImage.transform.rotation;
+
+        mTimer -= Time.deltaTime;
+        if (mTimer < 0f)
+        {
+            mTimer = Random.Range(30f, 45f);
+            mTimer = 5f;
+            int rotation = Random.Range(0, 4);
+            while (rotation == mCurrentRotation)
+                rotation = Random.Range(0, 4);
+
+            mMinimapRawImage.transform.DOLocalRotate(new Vector3(0f, 0f, rotation * 90f), 0.75f);
+        }
+    }
+}
