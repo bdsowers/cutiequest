7594de1fef6045dad6fd0052be82a768cf44b2c2
Brian Sowers <bdsowers@gmail.com>
2019-08-19 10:05:31 -0400

    Smoker quirk

---
+++ Assets/Scripts/Quirks/SmokerQuirk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokerQuirk : Quirk
+{
+    public GameObject smokePrefab;
+
+    private GameObject mSmoke;
+
+    private void Start()
+    {
+        mSmoke = GameObject.Instantiate(smokePrefab);    
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(mSmoke);
+    }
+}
---
+++ Assets/Scripts/Smoke.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    private bool mInDungeon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mInDungeon = Game.instance.InDungeon();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = Game.instance.avatar.transform.position;
+        pos.y = mInDungeon ? 2.5f : 3.5f;
+        transform.position = pos;
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+}
