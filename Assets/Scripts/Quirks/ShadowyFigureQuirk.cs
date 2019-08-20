54e7a68efa263bc25930c5db484a575ad6fb054e
Brian Sowers <bdsowers@gmail.com>
2019-08-20 15:08:38 -0400

    Adding a shady quirk, but cutting it immediately because it's not that impressive

---
+++ Assets/Scripts/Quirks/ShadowyFigureQuirk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowyFigureQuirk : Quirk
+{
+    public override void Start()
+    {
+        base.Start();
+
+        Game.instance.centralEvents.onEnemyCreated += OnEnemyCreated;
+    }
+
+    private void OnEnemyCreated(Enemy enemy)
+    {
+        enemy.GetComponentInChildren<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        Game.instance.centralEvents.onEnemyCreated -= OnEnemyCreated;
+    }
+}
