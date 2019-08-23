395935c075c31969d12c1378cfc7b0cfb833b6d6
Brian Sowers <bdsowers@gmail.com>
2019-08-23 14:12:23 -0400

    Minor tutorial touchup

--- Assets/Scripts/CinematicGame/CinematicActionSpawnPrefab.cs
+++ Assets/Scripts/CinematicGame/CinematicActionSpawnPrefab.cs
@@ -25,15 +25,18 @@ public class CinematicActionSpawnPrefab : CinematicAction
 
     public override IEnumerator PlayInternal(CinematicDirector player)
     {
+        GameObject.FindObjectOfType<CollisionMap>().MarkSpace(mMapX, mMapY, 1);
+
+        yield return new WaitForSeconds(Random.Range(0.2f, 0.4f));
+
         Vector3 worldCoords = MapCoordinateHelper.MapToWorldCoords(new Vector2Int(mMapX, mMapY));
         GameObject newObj = PrefabManager.instance.InstantiatePrefabByName(mPrefabName);
         newObj.transform.position = worldCoords;
 
         // todo bdsowers - this was rushed in for a cinematic
         newObj.transform.GetChild(0).localPosition = Vector3.up * 7f;
-        newObj.transform.GetChild(0).DOLocalMove(Vector3.zero, Random.Range(0.3f, 0.6f)).SetDelay(Random.Range(0.2f, 0.4f));
-        GameObject.FindObjectOfType<CollisionMap>().MarkSpace(mMapX, mMapY, 1);
-
+        newObj.transform.GetChild(0).DOLocalMove(Vector3.zero, Random.Range(0.3f, 0.6f));
+        
         yield break;
     }
 }
