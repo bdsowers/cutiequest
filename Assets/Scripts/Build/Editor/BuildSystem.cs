6959e670ce110cb56f5190584c6fde2f98b8c686
Brian Sowers <bdsowers@gmail.com>
2019-12-06 14:48:10 -0500

    First pass at build system

---
+++ Assets/Scripts/Build/Editor/BuildSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildSystem : MonoBehaviour
+{
+    private static string mSteamBuildLocation = "Steam/sdk/tools/ContentBuilder/content/HeroesSwipeRight.exe";
+    private static string mConventionBuildLocation = "Build/convention/HeroesSwipeRight.exe";
+
+    private static string BuildPath(string relativeTarget)
+    {
+        string outputPath = Application.dataPath;
+        DirectoryInfo di = new DirectoryInfo(outputPath);
+        string fullPath = Path.Combine(di.Parent.FullName, relativeTarget);
+        return fullPath;
+    }
+
+    public static void SteamBuild()
+    {
+        string outputPath = BuildPath(mSteamBuildLocation);
+
+        BuildOptions options = BuildOptions.None;
+        options |= BuildOptions.Development;
+        options |= BuildOptions.AllowDebugging;
+
+        List<string> flags = new List<string>();
+        flags.Add("DISABLE_CHEATS");
+
+        Build(outputPath, options, flags);
+    }
+
+    public static void ConventionBuild()
+    {
+        string outputPath = BuildPath(mConventionBuildLocation);
+
+        BuildOptions options = BuildOptions.None;
+        options |= BuildOptions.Development;
+        options |= BuildOptions.AllowDebugging;
+
+        List<string> flags = new List<string>();
+        flags.Add("DEMO");
+
+        Build(outputPath, options, flags);
+    }
+
+    public static void Build(string outputPath, BuildOptions options, List<string> flags)
+    {
+        string cacheSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+        string newSymbols = ConstructPreprocessorList(flags);
+        Debug.Log(newSymbols);
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, newSymbols);
+
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, outputPath, BuildTarget.StandaloneWindows, options);
+        Debug.Log("Build Complete: " + outputPath);
+
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, cacheSymbols);
+    }
+
+    private static string ConstructPreprocessorList(List<string> flags)
+    {
+        string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+        string[] currentFlags = symbols.Split(';');
+        List<string> finalFlags = new List<string>(currentFlags);
+        finalFlags.AddRange(flags);
+        return string.Join(";", finalFlags);
+    }
+}
---
+++ Assets/Scripts/Build/Editor/EditorBuildSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorBuildSystem : MonoBehaviour
+{
+    [MenuItem("HSR/Steam Build")]
+    public static void SteamBuild()
+    {
+        BuildSystem.SteamBuild();
+    }
+
+    [MenuItem("HSR/Convention Build")]
+    public static void ConventionBuild()
+    {
+        BuildSystem.ConventionBuild();
+    }
+}
