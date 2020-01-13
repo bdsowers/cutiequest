de2b5e102fa6c7bdaa593542f2684b3b0cc4b30b
Brian Sowers <bdsowers@gmail.com>
2020-01-13 11:15:56 -0500

    Second biome entrance

---
+++ Assets/Scripts/Editor/RandomizeYRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RandomizeYRotation : MonoBehaviour
+{
+    [MenuItem("HSR/Randomize Rotation")]
+    public static void RandomizeRotation()
+    {
+        GameObject[] objects = UnityEditor.Selection.gameObjects;
+        foreach(GameObject obj in objects)
+        {
+            Vector3 rotation = obj.transform.localRotation.eulerAngles;
+            rotation.y = Random.Range(0, 360f);
+            obj.transform.localRotation = Quaternion.Euler(rotation);
+        }
+    }
+}
