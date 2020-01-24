b0080228c55e91763eed5885ce9777aca33040ee
Brian Sowers <bdsowers@gmail.com>
2020-01-23 19:36:48 -0500

    New rock monster

---
+++ Assets/Scripts/GentleWobble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GentleWobble : MonoBehaviour
+{
+    Vector3 mStartPosition;
+    float mTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mStartPosition = transform.localPosition;
+
+        mTimer = Random.Range(0f, 50f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mTimer += Time.deltaTime;
+        transform.localPosition = mStartPosition + new Vector3(Mathf.Cos(mTimer), Mathf.Sin(mTimer * 2), 0f) * 0.05f;
+    }
+}
