1c27e89759c3324d282f9732173b1702990007d9
Brian Sowers <bdsowers@gmail.com>
2020-02-29 18:42:35 -0500

    Boss 2 tweaks

--- Assets/Scripts/EnemyAI/Boss2.cs
+++ Assets/Scripts/EnemyAI/Boss2.cs
@@ -142,10 +142,14 @@ public class Boss2 : EnemyAI
         else if (mCurrentPhase == 1)
         {
             mTeleport.teleportCooldown /= 2;
+            mSpell1.castSpeed *= 1.25f;
+            mSpell2.castSpeed *= 1.25f;
         }
         else if (mCurrentPhase == 2)
         {
             mTeleport.teleportCooldown /= 2;
+            mSpell1.castSpeed *= 1.25f;
+            mSpell2.castSpeed *= 1.25f;
         }
     }
 
