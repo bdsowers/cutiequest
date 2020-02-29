bc340e5b67fadc4c43fdc9d9c8e9e70a1ff27cff
Brian Sowers <bdsowers@gmail.com>
2020-02-29 10:22:28 -0500

    Reducing stat boosts

--- Assets/Scripts/CompanionBuilder.cs
+++ Assets/Scripts/CompanionBuilder.cs
@@ -87,7 +87,7 @@ public class CompanionBuilder : MonoBehaviour
         // Look at the player's current level in that area
         int baseLevel = Game.instance.avatar.GetComponent<CharacterStatistics>().BaseStatValue(statType);
 
-        return baseLevel / 2;
+        return baseLevel / 6;
     }
 
     private CharacterModelData PlayerModel()
