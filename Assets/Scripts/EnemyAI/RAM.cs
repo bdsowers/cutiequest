27cafea060497e0df5e64d47c4f52263b86442bd
Brian Sowers <bdsowers@gmail.com>
2020-02-29 15:54:12 -0500

    Changing ram character

---
+++ Assets/Scripts/EnemyAI/RAM.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ArrayExtensions;
+using DG.Tweening;
+using VectorExtensions;
+
+public class RAM : EnemyAI
+{
+    // Core actions:
+    //  * Teleport around wildly
+    //  * Teleport far away, charge forward, deal massive damage
+    //  * Teleport far away, cast screen-filling spell with narrow cut-out
+    //  * Cast screen-filling spell with rigid pattern
+
+    // Progression:
+    //  * Gets faster at each stage
+
+    Enemy mEnemy;
+
+    CharacterStatistics mStatistics;
+    Killable mKillable;
+    EnemyAI mCurrentActiveModule;
+    EnemyTeleport mTeleport;
+    SimpleMovement mSimpleMovement;
+    SimpleAttack mSimpleAttack;
+    Animator mAnimator;
+
+    public enum AIState
+    {
+        Wandering,
+        Teleporting,
+        Charging,
+        Stun,
+    }
+
+    int mCurrentPhase = 0;
+
+    private AIState mCurrentState = AIState.Teleporting;
+    private int mNumTeleports = 0;
+    private Vector3 mChargeDirection;
+    private float mStunTimer = 0f;
+
+    private bool mTeleportedIntoPlaceForState = false;
+
+    private void Start()
+    {
+        mEnemy = GetComponent<Enemy>();
+        mStatistics = GetComponent<CharacterStatistics>();
+        mKillable = GetComponent<Killable>();
+        mTeleport = GetComponent<EnemyTeleport>();
+        mSimpleMovement = GetComponent<SimpleMovement>();
+        mSimpleAttack = GetComponent<SimpleAttack>();
+        mAnimator = GetComponentInChildren<Animator>();
+
+        mEnemy.SetEnemyAI(this);
+
+        Game.instance.hud.bossHealth.gameObject.SetActive(true);
+        Game.instance.hud.bossHealth.SetWithValues(0, mKillable.health, mKillable.health);
+
+        Game.instance.hud.bossHealth.transform.localScale = Vector3.zero;
+        Game.instance.hud.bossHealth.transform.DOScale(1f, 0.5f);
+    }
+
+    private void OnEnable()
+    {
+        // Reveal the whole map when this boss becomes active
+        RevealWhenAvatarIsClose[] revealers = GameObject.FindObjectsOfType<RevealWhenAvatarIsClose>();
+        for (int i = 0; i < revealers.Length; ++i)
+        {
+            revealers[i].Reveal();
+        }
+    }
+
+    public override bool CanUpdateAI()
+    {
+        if (!this.enabled)
+            return false;
+        if (mKillable.isDead)
+            return false;
+        if (mSimpleMovement.isMoving)
+            return false;
+        if (mSimpleAttack.isAttacking)
+            return false;
+
+        return true;
+    }
+
+    public override void UpdateAI()
+    {
+        if (mCurrentState == AIState.Stun)
+        {
+            if (mStunTimer > 2f)
+            {
+                SwitchState();
+            }
+        }
+        else if (!mTeleportedIntoPlaceForState)
+        {
+            mTeleport.Teleport(true);
+
+            mTeleportedIntoPlaceForState = true;
+
+            // If we're going into the charging state, get ready to head toward the player ...
+            mChargeDirection = TowardPlayer();
+        }
+        else if (mCurrentState == AIState.Teleporting)
+        {
+            if (mTeleport.CanTeleport())
+            {
+                ++mNumTeleports;
+                int maxTeleports = 5 + mCurrentPhase;
+
+                mTeleport.Teleport();
+                SimpleMovement.OrientToDirection(mSimpleMovement.subMesh, TowardPlayer());
+
+                if (mNumTeleports >= maxTeleports)
+                {
+                    SwitchState();
+                }
+            }
+
+        }
+        else if (mCurrentState == AIState.Charging)
+        {
+            if (mSimpleAttack.CanAttack(mChargeDirection))
+            {
+                mSimpleAttack.Attack(mChargeDirection);
+                Camera.main.GetComponent<FollowCamera>().SimpleShake();
+
+                SwitchState();
+            }
+            else if (mSimpleMovement.CanMove(mChargeDirection))
+            {
+                mSimpleMovement.Move(mChargeDirection);
+            }
+            else
+            {
+                mStunTimer = 0f;
+                mCurrentState = AIState.Stun;
+
+                Camera.main.GetComponent<FollowCamera>().SimpleShake();
+            }
+        }
+    }
+
+    private void SwitchState()
+    {
+        AIState newState = mCurrentState;
+
+        // Don't repeat states
+        // Charging requires extra love since it transitions immediately into stun
+        while (newState == mCurrentState || (mCurrentState == AIState.Stun && newState == AIState.Charging))
+        {
+            int val = Random.Range(0, 4);
+            switch(val)
+            {
+                case 0: newState = AIState.Teleporting; break;
+                case 3: newState = AIState.Charging; break;
+            }
+        }
+
+        mCurrentState = newState;
+        mNumTeleports = 0;
+
+        Debug.Log(mCurrentState);
+
+        mTeleportedIntoPlaceForState = false;
+    }
+
+    private Vector3 TowardPlayer()
+    {
+        Vector3 towardPlayer = Game.instance.avatar.transform.position - transform.position;
+        towardPlayer.y = 0f;
+
+        if (Mathf.Abs(towardPlayer.x) > Mathf.Abs(towardPlayer.z))
+            towardPlayer.z = 0;
+        else
+            towardPlayer.x = 0;
+
+        if (towardPlayer.magnitude < 0.1f)
+            towardPlayer = Vector3.right;
+
+        towardPlayer.Normalize();
+        return towardPlayer;
+    }
+
+    public override void AIStructureChanged()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        if (Game.instance != null && Game.instance.hud != null)
+            Game.instance.hud.bossHealth.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (mCurrentState == AIState.Stun)
+        {
+            mStunTimer += Time.deltaTime;
+            mAnimator.Play("Dizzy");
+        }
+
+        Game.instance.hud.bossHealth.SetWithValues(0, mStatistics.ModifiedStatValue(CharacterStatType.MaxHealth, gameObject), mKillable.health);
+    }
+}
