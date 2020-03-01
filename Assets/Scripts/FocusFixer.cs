ea7a840a9c3a8e75f1daf7e65d137044c44141f7
Brian Sowers <bdsowers@gmail.com>
2020-03-01 16:28:46 -0500

    Fixing issue where the mouse could steal focus and never give it back

---
+++ Assets/Scripts/FocusFixer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+// Resolves a problem where the mouse could steal input and never give it back.
+public class FocusFixer : MonoBehaviour
+{
+    private GameObject mLastSelection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current == null)
+            return;
+
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            mLastSelection = EventSystem.current.currentSelectedGameObject;
+        }
+        else
+        {
+            if (mLastSelection != null && mLastSelection.activeInHierarchy)
+            {
+                EventSystem.current.SetSelectedGameObject(mLastSelection);
+            }
+        }
+    }
+}
