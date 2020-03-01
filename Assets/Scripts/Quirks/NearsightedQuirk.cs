b7274f39bd031d64c232e73a2233309e54cc16c9
Brian Sowers <bdsowers@gmail.com>
2020-03-01 16:17:14 -0500

    Monocle now cures nearsighted

--- Assets/Scripts/Quirks/NearsightedQuirk.cs
+++ Assets/Scripts/Quirks/NearsightedQuirk.cs
@@ -6,6 +6,12 @@ public class NearsightedQuirk : Quirk
 {
     public static float ApplyQuirkIfPresent(float range)
     {
+        // If Monocle is equipped, this is negated
+        if (Game.instance.playerStats.IsItemEquipped<Monocle>())
+        {
+            return range;
+        }
+
         if (Game.instance.quirkRegistry.IsQuirkActive<NearsightedQuirk>())
         {
             return range / 2;
