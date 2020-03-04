014ace2f62b5ad7422703e3fb84ea17b0aee0139
Brian Sowers <bdsowers@gmail.com>
2020-03-03 19:34:14 -0500

    Creating new FloorTile component to track removable decorations

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile : MonoBehaviour
+{
+    public List<GameObject> removableDecroations;
+
+    public void RemoveDecorations()
+    {
+        for (int i = 0; i < removableDecroations.Count; ++i)
+        {
+            removableDecroations[i].SetActive(false);
+        }
+    }
+}
