faf53eb23def2e4b222e8242101757d2aa5a647a
Brian Sowers <bdsowers@gmail.com>
2020-03-05 10:25:28 -0500

    Attempting fix for input lock bug

---
+++ Assets/Scripts/InputPatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+/// <summary>
+/// Combats a rare but nasty bug where InControl freezes input
+/// </summary>
+public class InputPatch : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Game.instance != null)
+            InputManager.ClearInputState();
+    }
+}
