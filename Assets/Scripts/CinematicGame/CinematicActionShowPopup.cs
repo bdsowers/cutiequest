55471a2715678509e7ebd5e4cba193a7b8576296
Brian Sowers <bdsowers@gmail.com>
2020-03-05 19:44:01 -0500

    Better feedback when not enough money

---
+++ Assets/Scripts/CinematicGame/CinematicActionShowPopup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicActionShowPopup : CinematicAction
+{
+    public override string actionName
+    {
+        get { return "show_popup"; }
+    }
+
+    public override string simpleParameterName
+    {
+        get { return "message"; }
+    }
+
+    private string mMessage;
+
+    public override void InterpretParameters(CinematicDataProvider dataProvider)
+    {
+        base.InterpretParameters(dataProvider);
+
+        mMessage = dataProvider.GetStringData(mParameters, "message");
+    }
+
+    public override IEnumerator PlayInternal(CinematicDirector player)
+    {
+        NumberPopupGenerator.instance.GeneratePopup(Game.instance.avatar.gameObject, mMessage, NumberPopupReason.Bad, 0f);
+
+        yield break;
+    }
+}
